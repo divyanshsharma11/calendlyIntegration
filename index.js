fe2dadd3fb4a46d2e56498239452bce0aff4c6c5
Divyansh <divyanshsharma1121@gmail.com>
2025-11-29 18:12:09 +0530

    DEV: code changes to add loggers,try catch,and fixing bugs

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const URL = process.env.PUBLIC_BASE_URL || `http://localhost:${PORT}`;
 
 server.listen(PORT, () => {
   logger.info(`Server running at ${URL}`);
-  logger.info(`Swagger UI available at ${URL}/api/docs`);
+  logger.info(`Swagger UI available at ${URL}api/docs`);
 });
 
 module.exports = app;
