48951cee5509734a39861e7ba4c54681f17b5b72
Divyansh <divyanshsharma1121@gmail.com>
2025-12-01 15:15:05 +0530

    DEV: correcting httpClient code

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,127 +1,34 @@
 const axios = require("axios");
-const UserToken = require("../../src/models/UserToken");
-const { Logger } = require("../../src/utils/logger");
-const { errorFormat } = require("../utils/response");
-const {
-  ENTERING,
-  SERVICE_METHOD,
-  METHODS,
-  UTILS,
-} = require("../../src/constants/constants");
-const { calendlyRequest } = require("./httpClient");
+const retry = require("./retry");
+const { Logger } = require("./logger");
+const { ENTERING, METHODS, UTILS } = require("../constants/constants");
 
-// --- Fetch ONE page of scheduled events ---
-const fetchEventsPage = async (accessToken, userUri, nextCursor = null) => {
-  const logger = new Logger(
-    `${ENTERING} ${UTILS} ${METHODS.SYNC.FETCH_EVENTS_PAGE}`
-  );
+const logger = new Logger(
+  `${ENTERING} ${UTILS} ${METHODS.UTILS.UTILS_HTTP_CLIENT}`
+);
 
-  try {
-    const params = {
-      count: 20,
-      user: userUri,
-    };
+// Base Calendly API client
+const calendlyClient = axios.create({
+  baseURL: process.env.CALENDLY_API_BASE || "https://api.calendly.com",
+  timeout: 15000,
+});
 
-    if (nextCursor) params.page_token = nextCursor;
-
-    const response = await calendlyRequest({
-      method: "GET",
-      url: "/scheduled_events",
-      params,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    logger.error(`Error in fetchEventsPage: ${error.message}`);
-    return Promise.reject(errorFormat(error));
-  }
-};
-
-// --- Fetch ALL scheduled events with pagination ---
-const fetchAllEvents = async ({ pageLimit = 5 }) => {
-  const logger = new Logger(
-    `${ENTERING} ${UTILS} ${METHODS.SYNC.FETCH_ALL_EVENTS}`
-  );
-
-  try {
-    logger.info(` pageLimit | ${pageLimit}`);
-
-    const tokenDoc = await UserToken.findOne().lean();
-    if (!tokenDoc?.access_token) {
-      throw new Error("Missing OAuth access token");
+// Wrapper to apply retry for ALL Calendly requests
+async function calendlyRequest(config) {
+  return retry(
+    async () => {
+      logger.info(`Calendly API Request | ${JSON.stringify(config)}`);
+      const res = await calendlyClient(config);
+      return res.data;
+    },
+    {
+      retries: 3,
+      delay: 300,
+      retryStatusCodes: [429, 500, 502, 503, 504],
     }
-
-    const accessToken = tokenDoc.access_token;
-    const userUri = tokenDoc.userURI;
-
-    logger.info(`accessToken || ${JSON.stringify(accessToken)}`);
-    logger.info(`userUri || ${JSON.stringify(userUri)}`);
-
-    let allEvents = [];
-    let nextCursor = null;
-    let pagesFetched = 0;
-
-    do {
-      const page = await fetchEventsPage(accessToken, userUri, nextCursor);
-      logger.info(`page || ${JSON.stringify(page)}`);
-
-      const eventsList = page.collection || [];
-      allEvents = allEvents.concat(eventsList);
-
-      nextCursor = page.pagination?.next_page_token || null;
-      pagesFetched++;
-
-      if (!nextCursor || pagesFetched >= pageLimit) break;
-    } while (true);
-
-    logger.info(
-      `Fetched ${allEvents.length} events across ${pagesFetched} pages`
-    );
-
-    return allEvents;
-  } catch (error) {
-    logger.error(`Calendly Fetch Events Error | ${error.message}`);
-    return Promise.reject(errorFormat(error));
-  }
-};
-
-const createWebhookSubscription = async (accessToken, payload) => {
-  const logger = new Logger(
-    `${ENTERING} ${UTILS} ${METHODS.WEBHOOKS.CREATE_WEBHOOK_SUBSCRIPTION}`
   );
-
-  try {
-    logger.info(` webhook payload | ${JSON.stringify(payload)}`);
-
-    const response = await calendlyRequest({
-      method: "POST",
-      url: "/webhook_subscriptions",
-      payload,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-    });
-
-    logger.info(
-      `Calendly webhook subscription created | ${JSON.stringify(response.data)}`
-    );
-
-    return response.data?.resource;
-  } catch (error) {
-    logger.error(
-      `Calendly Create Webhook Error | ${error.message} | ${JSON.stringify(
-        error?.response?.data
-      )}`
-    );
-    return Promise.reject(errorFormat(error));
-  }
-};
+}
 
 module.exports = {
-  fetchAllEvents,
-  createWebhookSubscription,
+  calendlyRequest,
 };
